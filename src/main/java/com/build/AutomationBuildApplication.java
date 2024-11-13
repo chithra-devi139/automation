3976ebe9b186d3f2e7e8c9a567618727918b4cda
chithra-devi139 <chitrasumithra139@gmail.com>
2024-11-13 17:13:27 +0530

    auto-commit

---
+++ src/main/java/com/build/AutomationBuildApplication.java
@@ -0,0 +1,13 @@
+package com.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationBuildApplication.class, args);
+	}
+
+}
